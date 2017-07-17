89e0d3362b86cae5cc2bc5ebdeddf6dc6e73fe70
Scott <sdmccoy@gmail.com>
2017-07-17 14:29:18 -0700

    added react component

---
+++ lab26-scott/src/main.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import faker from 'faker';
+import cowsay from 'cowsay-browser';
+
+//create the App component where everything will go.
+
+class App extends React.Component{
+  //what I want to be rendered on the DOM
+  render(){
+  //always wrap it in a div
+    return (
+    <div>
+    <h1>'hello world'</h1>
+    </div>
+    );
+  };
+};
+
+//use render method on reactdom to render the DOM
+ReactDOM.render(<App />, document.getElementById('root'));
