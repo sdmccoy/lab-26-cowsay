90b11c883efe810bd6ca29c9bfa036c555c06b7d
Scott <sdmccoy@gmail.com>
2017-07-17 15:03:25 -0700

    constructor and button handler built

--- lab26-scott/src/main.js
+++ lab26-scott/src/main.js
@@ -6,12 +6,35 @@ import cowsay from 'cowsay-browser';
 //create the App component where everything will go.
 
 class App extends React.Component{
+  //start the constructor and pass through props, props is the element properties
+  constructor(props){
+    //superprops allows us to manage state
+    super(props);
+    //create a state object for state instances
+    this.state = {
+      cowsay: '',
+    }
+  };
+
+  //handler method for the click action on button
+  handleButtonClick(e){
+    //set the state every time the button is clicked
+    this.setState(state => {
+      return {
+        cowsay: cowsay.say({
+          text: faker.lorem.words(10),
+        }) 
+      };
+    });
+  };
+
   //what I want to be rendered on the DOM
   render(){
   //always wrap it in a div
     return (
     <div>
-      <h1>hello world</h1>
+      <h1>Generate Cowsay Lorem</h1>
+      <button onClick={this.handleButtonClick}>click me</button>
     </div>
     );
   };
